86ce8206425ea9d35c95275ef87662d17f64ed49
Marek <marekz@gmail.com>
2015-06-21 15:25:20 +0000

    Hotfix attempt to solve 401 errors

--- app/models/box_access.rb
+++ app/models/box_access.rb
@@ -1,7 +1,7 @@
 class BoxAccess < ActiveRecord::Base
     
     def needs_refresh?
-        last_refresh.nil? || last_refresh <= 1.hour.ago
+        true# last_refresh.nil? || last_refresh <= 1.hour.ago
     end
     
 end
